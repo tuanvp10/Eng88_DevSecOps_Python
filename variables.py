351a24f277089feecc25723a6673ab04e4fb45f6
tuanvp10 <83917685+tuanvp10@users.noreply.github.com>
2021-05-18 16:54:23 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Let's create a variable for first name, last name and DOB
+# Syntax name of the variable = value to store in the variable
+# Naming Convention to follow
+
+first_name = "Tuan" # String
+
+last_name = "Pham" # String
+
+Salary = 1233 # Integer
+
+float_value = 112.12 # Float numbers
+
+print(first_name)
+print(last_name)
+print(Salary)
+print(float_value)
+
+# Taking user input - we have a method called input()
+print("Please enter your name ")
+name = input(" ")
+print("Hello")
+print(name)
+
+# To find out the type of variable we have method called type()
+print(type(float_value))
+print(type(Salary))
+print(type(first_name))
